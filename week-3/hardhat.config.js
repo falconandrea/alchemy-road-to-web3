f207b2ad2d5c5b4bf86d0abfd563ee03b012d735
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-11-18 20:34:27 +0100

    feat: add etherscan and network in hardhat config

--- week-3/hardhat.config.js
+++ week-3/hardhat.config.js
@@ -1,6 +1,17 @@
+require('dotenv').config()
 require('@nomicfoundation/hardhat-toolbox')
+require('@nomiclabs/hardhat-etherscan')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.17'
+  solidity: '0.8.17',
+  networks: {
+    mumbai: {
+      url: process.env.NETWORK_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 }
