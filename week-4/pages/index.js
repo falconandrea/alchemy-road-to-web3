dcad9c92a51d03c3d83038f77b9bbaffab7a9ee0
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-11-21 19:05:19 +0100

    feat: init week-4 with new nextjs app

---
+++ week-4/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home () {
+  return (
+    <div>
+      <Head>
+        <title>NFT Gallery</title>
+        <meta name='description' content='Simple NFT gallery' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>Start</main>
+    </div>
+  )
+}
